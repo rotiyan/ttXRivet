df5d53d31b2f1b1bf2a850efda7ea92885cfe825
narayan <rohin.thampilali.narayan@cern.ch>
2015-11-11 15:12:20 +0100

    changed output sample naming scheme

--- rivet_ana/gridsub_phase2.sh
+++ rivet_ana/gridsub_phase2.sh
@@ -1,9 +1,10 @@
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/
 export ALRB_localConfigDir=/code/localConfig
 . /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh
-localSetupPandaClient
-asetup 19.2.3.9,here 
+lsetup asetup
+lsetup panda
+asetup 19.3.0.1,here 
 
 timeStamp=`date +%H%M%S_%m%Y`
 
-pathena -c 'xs=0.054658' --noBuild --extFile=RivetTTBAR_ANA.so,RivetTTZ_analysis.so --nFilesPerJob=2 --long --extOutFile=ttz_analysis.yoda --inDS=user.mcfayden.evnt.2015-08-14_111245.410000.13TeV_ttll_EXT1/ --outDS=user.narayan.${timeStamp}.rivet-MadGraph_MC_ANA.410000_13TeV_ttll_nonDressed/ rivet_MC_jo.py
+pathena -c 'xs=0.054658' --noBuild --extFile=RivetTTBAR_ANA.so,RivetTTZ_analysis.so --nFilesPerJob=2 --long --extOutFile=ttz_analysis.yoda --inDS=user.mcfayden.evnt.2015-08-14_111245.410000.13TeV_ttll_EXT1/ --outDS=user.narayan.${timeStamp}.rivet-MadGraph_MC_ANA.410000_13TeV_ttll_Phase2_nonDressed/ rivet_MC_jo.py
