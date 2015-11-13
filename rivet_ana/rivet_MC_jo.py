7764583636b86bf1de3cb7081c1d307aee782fe0
Rohin Narayan <narayan@tau4.hep.utexas.edu>
2015-11-13 04:25:57 -0600

    jo change

--- rivet_ana/rivet_MC_jo.py
+++ rivet_ana/rivet_MC_jo.py
@@ -1,13 +1,18 @@
 from glob import glob
 
 
-#theApp.EvtMax = -1
-theApp.EvtMax=500
+theApp.EvtMax = -1
+#theApp.EvtMax=500
 
 import AthenaPoolCnvSvc.ReadAthenaPool
 #svcMgr.EventSelector.InputCollections = glob("~/samples/user.mcfayden.evnt.2015-05-07_025309.200001.8TeV_ttZlloff_EXT1/*.root*")
 #svcMgr.EventSelector.InputCollections=['/code/rohin/mcvalid/josh_jo/WorkArea/run/MadgraphControl_ttV_LO_Pythia8_A14_CKKWLkTMerge.pool.root']
-svcMgr.EventSelector.InputCollections=['/home/narayan/samples/user.mcfayden.evnt.2015-05-07_025309.200001.8TeV_ttZlloff_EXT1/user.mcfayden.5405795.EXT1._000186.mc12_7TeV.200001.EVNT.root']
+#svcMgr.EventSelector.InputCollections=['/home/narayan/samples/user.mcfayden.evnt.2015-05-07_025309.200001.8TeV_ttZlloff_EXT1/user.mcfayden.5405795.EXT1._000186.mc12_7TeV.200001.EVNT.root']
+svcMgr.EventSelector.InputCollections = glob("/data/onyisi/sherpa/ttll_hi_13TeV/*")
+#svcMgr.EventSelector.InputCollections = glob('/data/narayan/josh_samples_massive/user.mcfayden.evnt.2015-10-25_182608.410111.13TeV_ttee/*')
+#svcMgr.EventSelector.InputCollections = glob('/data/narayan/josh_samples_massive/user.mcfayden.evnt.2015-10-25_182708.410112.13TeV_ttee/*')
+#svcMgr.EventSelector.InputCollections = glob('/data/narayan/josh_samples_massive/user.mcfayden.evnt.2015-10-25_182804.410113.13TeV_ttmumu/*')
+#svcMgr.EventSelector.InputCollections = glob('/data/narayan/josh_samples_massive/user.mcfayden.evnt.2015-10-25_182859.410114.13TeV_ttmumu/*')
 from AthenaCommon.AlgSequence import AlgSequence
 job = AlgSequence()
 
