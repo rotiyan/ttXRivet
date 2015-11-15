227d2203106c4367f10aa545496b0c45a13138b0
narayan <rohin.thampilali.narayan@cern.ch>
2015-11-15 22:18:15 +0100

    changes to TTZ analysis

--- rivet_plugins/TTBAR_ANA.cc
+++ rivet_plugins/TTBAR_ANA.cc
@@ -46,8 +46,8 @@ namespace Rivet {
 
       FinalState zfs;
       Cut zeefscut = (Cuts::abseta <4.2 )& (Cuts::pT>10*GeV);
-      ZFinder zeeFinder(zfs,zeefscut,PID::ELECTRON,5.0*GeV,115.0*GeV,0.2,ZFinder::NOCLUSTER,ZFinder::NOTRACK);
-      //ZFinder zeeFinder(zfs,zeefscut,PID::ELECTRON,5.0*GeV,115.0*GeV,0.2,ZFinder::CLUSTERNODECAY,ZFinder::NOTRACK);
+      //ZFinder zeeFinder(zfs,zeefscut,PID::ELECTRON,5.0*GeV,115.0*GeV,0.2,ZFinder::NOCLUSTER,ZFinder::NOTRACK);
+      ZFinder zeeFinder(zfs,zeefscut,PID::ELECTRON,5.0*GeV,115.0*GeV,0.2,ZFinder::CLUSTERNODECAY,ZFinder::NOTRACK);
       //ZFinder zeeFinder(zfs,zeefscut,PID::ELECTRON,5.0*GeV,115.0*GeV,0.1,ZFinder::CLUSTERALL,ZFinder::TRACK);
       addProjection(zeeFinder,"zeeFinder");
 
