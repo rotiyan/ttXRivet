71d4c1a93dd7fd3e170d12b5e2b07bf4bfb83571
Rohin Thampilali Narayan <narayan@lxplus027.cern.ch>
2018-07-25 12:39:33 +0200

    adding ttw analysis case from maria

--- rivet_plugins/ttw_analysis.cc
+++ rivet_plugins/ttw_analysis.cc
@@ -18,6 +18,10 @@
 
 
 
+//MARIA (ToDo)
+// RIVETanalysis: add histos for ttW_pt/eta/rap, tt_eta/rap (pt already exists)
+// For the analysis, will spit into two ttW channels: 3l (with dilepton tt: TopDil_Lep1, TopDil_Lep2) and 2lSS (with lep+jets tt: TopLJets_Lep, TopLJets_Had)
+
 
 /// @todo Include more projections as required, e.g. ChargedFinalState, FastJets, ZFinder...
 
@@ -51,6 +55,8 @@ namespace Rivet {
         _h_wPlus_nJets = bookHisto1D("wPlus_nJets",11,-0.5,10.5);
         _h_wMinus_nJets= bookHisto1D("wMinus_nJets",11,-0.5,10.5);
         _h_nJets    = bookHisto1D("event_nJets",11,-0.5,10.5);
+	    _h_matchedjets_N = bookHisto1D("event_nMatchedJets",11,-0.5,10.5);
+	    _h_matchedjets_pT    = bookHisto1D("matchedjets_pT",100,50,450);
         _h_ttbar_pt = bookHisto1D("ttbar_pt",150,130,430);
         _h_t1_mass  = bookHisto1D("t1_mass",150,130,430);
         _h_t2_mass  = bookHisto1D("t2_mass",150,130,430);
@@ -75,6 +81,25 @@ namespace Rivet {
         _h_w1_phi   = bookHisto1D("w1_phi",100,-5,5);
         _h_w2_phi   = bookHisto1D("w2_phi",100,-5,5);
 
+	//MARIA
+        _h_lep1_mass  = bookHisto1D("lep1_mass",100,0,120);
+        _h_lep2_mass  = bookHisto1D("lep2_mass",100,0,120);
+        _h_lep1_pt    = bookHisto1D("lep1_pt",100,0,400);
+        _h_lep2_pt    = bookHisto1D("lep2_pt",100,0,400);
+        _h_lep1_eta   = bookHisto1D("lep1_eta",100,-5,5);
+        _h_lep2_eta   = bookHisto1D("lep2_eta",100,-5,5);
+        _h_lep1_phi   = bookHisto1D("lep1_phi",100,-5,5);
+        _h_lep2_phi   = bookHisto1D("lep2_phi",100,-5,5);
+
+        _h_lightq1_mass  = bookHisto1D("lightq1_mass",100,0,120);
+        _h_lightq2_mass  = bookHisto1D("lightq2_mass",100,0,120);
+        _h_lightq1_pt    = bookHisto1D("lightq1_pt",100,0,400);
+        _h_lightq2_pt    = bookHisto1D("lightq2_pt",100,0,400);
+        _h_lightq1_eta   = bookHisto1D("lightq1_eta",100,-5,5);
+        _h_lightq2_eta   = bookHisto1D("lightq2_eta",100,-5,5);
+        _h_lightq1_phi   = bookHisto1D("lightq1_phi",100,-5,5);
+        _h_lightq2_phi   = bookHisto1D("lightq2_phi",100,-5,5);
+   
         
         _h_e_dEta   = bookHisto1D("zel_dEta",100,0,7);
         _h_e_dPhi   = bookHisto1D("zel_dPhi",100,0,7);
@@ -91,6 +116,14 @@ namespace Rivet {
         _h_w_dEta   = bookHisto1D("w_dEta",100,0,7);
         _h_w_dPhi   = bookHisto1D("w_dPhi",100,0,7);
         _h_w_dR     = bookHisto1D("w_dR",100,0,7);
+
+	//MARIA
+        _h_lep_dEta   = bookHisto1D("lep_dEta",100,0,7);
+        _h_lep_dPhi   = bookHisto1D("lep_dPhi",100,0,7);
+        _h_lep_dR     = bookHisto1D("lep_dR",100,0,7);
+        _h_lightq_dEta   = bookHisto1D("lightq_dEta",100,0,7);
+        _h_lightq_dPhi   = bookHisto1D("lightq_dPhi",100,0,7);
+        _h_lightq_dR     = bookHisto1D("lightq_dR",100,0,7);
         
         _h_b_dEta   = bookHisto1D("b_dEta",100,0,7);
         _h_b_dPhi   = bookHisto1D("b_dPhi",100,0,7);
@@ -112,18 +145,6 @@ namespace Rivet {
     void analyze(const Event& event) {
 
       const double weight = event.weight();
-
-
-      const MissingMomentum &met= applyProjection<MissingMomentum>(event,"MissingET");
-      const double event_met    = met.vectorEt().mod()*GeV;
-  
-      
-      const FastJets & jetProj  = applyProjection<FastJets>(event,"Jets");
-      const Jets alljets        = jetProj.jetsByPt(25*GeV);
-      double event_ht           = 0;
-      foreach(const Jet &j, alljets){ event_ht += j.pT()*GeV;}
-
-      _h_nJets->fill(alljets.size(),weight);
 
       MSG_INFO("#----------------Event--------------#");
 
@@ -151,7 +172,7 @@ namespace Rivet {
           }
       }
       
-      Particles wBosons, wBosonsME, topQuarks, bQuarks;
+      Particles wBosons, wBosonsME, topQuarks, bQuarks, LepfromWTop, LightQuarkfromWTop;
 
       foreach (const Particle &W, wCands)
       {
@@ -190,6 +211,7 @@ namespace Rivet {
       foreach (const Particle &W, wCands)
       {
           bool wlepDecay = false;
+	      bool whadDecay = false; //MARIA
           bool noTopParent = false;
           //GenVertex *wVert      = W.genParticle()->production_vertex();
           GenVertex *wEndVert   = W.genParticle()->end_vertex();
@@ -218,30 +240,85 @@ namespace Rivet {
               if(wchPdg ==11 || wchPdg ==12 || wchPdg ==13 || wchPdg ==14) 
               {
                   wlepDecay = true;
+		         if (!noTopParent) 
+                 {
+                    LepfromWTop.push_back(*witer);//MARIA (two entries per event)
+                    break; //Why break ?
+                 }
+              }
+	          else if(wchPdg ==1 || wchPdg ==2 || wchPdg ==3 || wchPdg ==4) { //MARIA (two entries per event)
+                  whadDecay = true;
+		          LightQuarkfromWTop.push_back(*witer);//will match with particle-level jets
                   break;
               }
           }
+
+	      //associated W from ME
           if(wlepDecay && noTopParent)
           {
               wBosonsME.push_back(W);
           }
-      }
-
+	  
+      }//end Wcand loop
+    
       sortByPt(topQuarks);
       sortByPt(bQuarks);
       sortByPt(wBosons);
       sortByPt(wBosonsME);
+      sortByPt(LepfromWTop);//MARIA
+      sortByPt(LightQuarkfromWTop);//MARIA
 
-      if(topQuarks.size()==2 && wBosons.size()==2 && bQuarks.size()==2 && wBosonsME.size()==1)
+
+
+      const MissingMomentum &met= applyProjection<MissingMomentum>(event,"MissingET");
+      const double event_met    = met.vectorEt().mod()*GeV;
+  
+      
+      const FastJets & jetProj  = applyProjection<FastJets>(event,"Jets");
+      const Jets alljets        = jetProj.jetsByPt(25*GeV);
+      double event_ht           = 0;
+      foreach(const Jet &j, alljets){ event_ht += j.pT()*GeV;}
+
+      _h_nJets->fill(alljets.size(),weight);
+
+      // dR (quarks & jets) -- bQuarks,
+      Jets _lightjetsMatchedToLightQ; 
+      foreach (const Jet& _jet, alljets) {
+	if (fabs(_jet.eta()) < 2.5) {
+	  // jet B-hadron matching
+	  foreach (const Particle &quark, LightQuarkfromWTop) {
+	    FourMomentum _quarkfm = quark.momentum();
+	    double _quark_jet_dR = deltaR(_jet.momentum(), _quarkfm);
+	    if(_quark_jet_dR < 0.4) {
+	      _lightjetsMatchedToLightQ.push_back(_jet);
+	      _h_matchedjets_pT->fill(_jet.momentum().pT()/GeV, weight);
+	    }
+	    continue;
+	  }
+	}
+      }
+      _h_matchedjets_N->fill(_lightjetsMatchedToLightQ.size(),weight);
+
+    
+      MSG_INFO("WCandSize: "<<wCands.size() );
+      MSG_INFO("wBosonsSize: "<<wBosons.size());
+      MSG_INFO("bQuarksSize: "<<bQuarks.size());
+      MSG_INFO("wBosonsMESize" <<wBosonsME.size());
+      MSG_INFO("LepFromWTOP" <<LepfromWTop.size());
+      MSG_INFO("LightQuarkfromWTop" <<LightQuarkfromWTop.size());
+
+      if(topQuarks.size()==2 && wBosons.size()==2 && bQuarks.size()==2 && wBosonsME.size()==1 && (LepfromWTop.size() >=1 && LightQuarkfromWTop.size() >=2 ) )
       {
           MSG_INFO("WCANDS size: "<< wCands.size());
           MSG_INFO("WBOSONSME Size: "<<wBosonsME.size());
           MSG_INFO("WBOSONS Size: "<<wBosons.size());
           MSG_INFO("bQuarks Size: "<<wBosons.size());
           MSG_INFO("topQuarks Size: "<<topQuarks.size());
+	      MSG_INFO("LepfromWTop Size: "<<LepfromWTop.size());
+	      MSG_INFO("LightQuarkfromWTop Size: "<<LightQuarkfromWTop.size());
       }
 
-      if(topQuarks.size()==2 && wBosons.size()==2 && bQuarks.size()==2 && wBosonsME.size()==1)
+      if(topQuarks.size()==2 && wBosons.size()==2 && bQuarks.size()==2 && wBosonsME.size()==1 && (LepfromWTop.size() >=1 && LightQuarkfromWTop.size() >=2) )
       {
           _h_top_dEta->fill(deltaEta(topQuarks[0],topQuarks[1]),weight);
           _h_top_dPhi->fill(deltaPhi(topQuarks[0],topQuarks[1]),weight);
@@ -279,6 +356,32 @@ namespace Rivet {
           _h_w2_pt->fill(wBosons[1].momentum().perp()*GeV,weight);
           _h_w2_eta->fill(wBosons[1].momentum().eta(),weight);
           _h_w2_phi->fill(wBosons[1].momentum().phi(),weight);
+
+	  //MARIA
+          _h_lep_dEta->fill(deltaEta(LepfromWTop[0],LepfromWTop[1]),weight);
+          _h_lep_dPhi->fill(deltaPhi(LepfromWTop[0],LepfromWTop[1]),weight);
+          _h_lep_dR->fill(deltaR(LepfromWTop[0],LepfromWTop[1]),weight);
+          _h_lep1_mass->fill(LepfromWTop[0].momentum().mass()*GeV,weight);
+          _h_lep1_pt->fill(LepfromWTop[0].momentum().perp()*GeV,weight);
+          _h_lep1_eta->fill(LepfromWTop[0].momentum().eta(),weight);
+          _h_lep1_phi->fill(LepfromWTop[0].momentum().phi(),weight);
+          _h_lep2_mass->fill(LepfromWTop[1].momentum().mass()*GeV,weight);
+          _h_lep2_pt->fill(LepfromWTop[1].momentum().perp()*GeV,weight);
+          _h_lep2_eta->fill(LepfromWTop[1].momentum().eta(),weight);
+          _h_lep2_phi->fill(LepfromWTop[1].momentum().phi(),weight);
+
+	  //MARIA
+          _h_lightq_dEta->fill(deltaEta(LightQuarkfromWTop[0],LightQuarkfromWTop[1]),weight);
+          _h_lightq_dPhi->fill(deltaPhi(LightQuarkfromWTop[0],LightQuarkfromWTop[1]),weight);
+          _h_lightq_dR->fill(deltaR(LightQuarkfromWTop[0],LightQuarkfromWTop[1]),weight);
+          _h_lightq1_mass->fill(LightQuarkfromWTop[0].momentum().mass()*GeV,weight);
+          _h_lightq1_pt->fill(LightQuarkfromWTop[0].momentum().perp()*GeV,weight);
+          _h_lightq1_eta->fill(LightQuarkfromWTop[0].momentum().eta(),weight);
+          _h_lightq1_phi->fill(LightQuarkfromWTop[0].momentum().phi(),weight);
+          _h_lightq2_mass->fill(LightQuarkfromWTop[1].momentum().mass()*GeV,weight);
+          _h_lightq2_pt->fill(LightQuarkfromWTop[1].momentum().perp()*GeV,weight);
+          _h_lightq2_eta->fill(LightQuarkfromWTop[1].momentum().eta(),weight);
+          _h_lightq2_phi->fill(LightQuarkfromWTop[1].momentum().phi(),weight);
 
 
           if(wBosonsME[0].genParticle()->pdg_id()==24)
@@ -338,6 +441,24 @@ namespace Rivet {
         scale(_h_w1_phi,norm);
         scale(_h_w2_phi,norm);
 
+	//MARIA
+        scale(_h_lep1_mass,norm);
+        scale(_h_lep2_mass,norm);
+        scale(_h_lep1_pt,norm);
+        scale(_h_lep2_pt,norm);
+        scale(_h_lep1_eta,norm);
+        scale(_h_lep2_eta,norm);
+        scale(_h_lep1_phi,norm);
+        scale(_h_lep2_phi,norm);
+        scale(_h_lightq1_mass,norm);
+        scale(_h_lightq2_mass,norm);
+        scale(_h_lightq1_pt,norm);
+        scale(_h_lightq2_pt,norm);
+        scale(_h_lightq1_eta,norm);
+        scale(_h_lightq2_eta,norm);
+        scale(_h_lightq1_phi,norm);
+        scale(_h_lightq2_phi,norm);
+
         //Delta distributions
         scale(_h_e_dEta,norm);
         scale(_h_e_dPhi,norm);
@@ -355,6 +476,13 @@ namespace Rivet {
         scale(_h_w_dEta,norm);
         scale(_h_w_dPhi,norm);
         scale(_h_w_dR,norm);
+	//MARIA
+        scale(_h_lep_dEta,norm);
+        scale(_h_lep_dPhi,norm);
+        scale(_h_lep_dR,norm);
+        scale(_h_lightq_dEta,norm);
+        scale(_h_lightq_dPhi,norm);
+        scale(_h_lightq_dR,norm);
 
         scale(_h_wPlusME_pt,norm);
         scale(_h_wPlusME_eta,norm);
@@ -362,6 +490,10 @@ namespace Rivet {
         scale(_h_wMinusME_pt,norm);
         scale(_h_wMinusME_eta,norm);
         scale(_h_wMinusME_phi,norm);
+
+	    scale(_h_nJets,norm);
+	    scale(_h_matchedjets_N,norm);
+	    scale(_h_matchedjets_pT,norm);
     }
     //@}
 
@@ -372,6 +504,7 @@ namespace Rivet {
     Histo1DPtr _h_wPlus_MET,_h_wMinus_MET;
     Histo1DPtr _h_wPlus_HT,_h_wMinus_HT;
     Histo1DPtr _h_wPlus_nJets,_h_wMinus_nJets,_h_nJets;
+    Histo1DPtr _h_matchedjets_N,_h_matchedjets_pT;
     Histo1DPtr _h_ttbar_pt;
     Histo1DPtr _h_t1_mass;
     Histo1DPtr _h_t2_mass;
@@ -396,6 +529,25 @@ namespace Rivet {
     Histo1DPtr _h_w1_phi;
     Histo1DPtr _h_w2_phi;
 
+    //MARIA
+    Histo1DPtr _h_lep1_mass;
+    Histo1DPtr _h_lep2_mass;
+    Histo1DPtr _h_lep1_pt;
+    Histo1DPtr _h_lep2_pt;
+    Histo1DPtr _h_lep1_eta;
+    Histo1DPtr _h_lep2_eta;
+    Histo1DPtr _h_lep1_phi;
+    Histo1DPtr _h_lep2_phi;
+
+    Histo1DPtr _h_lightq1_mass;
+    Histo1DPtr _h_lightq2_mass;
+    Histo1DPtr _h_lightq1_pt;
+    Histo1DPtr _h_lightq2_pt;
+    Histo1DPtr _h_lightq1_eta;
+    Histo1DPtr _h_lightq2_eta;
+    Histo1DPtr _h_lightq1_phi;
+    Histo1DPtr _h_lightq2_phi;
+
     Histo1DPtr _h_e_dEta;
     Histo1DPtr _h_e_dPhi;
     Histo1DPtr _h_e_dR;
@@ -412,6 +564,14 @@ namespace Rivet {
     Histo1DPtr _h_w_dEta;
     Histo1DPtr _h_w_dPhi;
     Histo1DPtr _h_w_dR;
+
+    //MARIA
+    Histo1DPtr _h_lep_dEta;
+    Histo1DPtr _h_lep_dPhi;
+    Histo1DPtr _h_lep_dR;
+    Histo1DPtr _h_lightq_dEta;
+    Histo1DPtr _h_lightq_dPhi;
+    Histo1DPtr _h_lightq_dR;
 
     Histo1DPtr _h_wPlusME_pt;
     Histo1DPtr _h_wPlusME_eta;
